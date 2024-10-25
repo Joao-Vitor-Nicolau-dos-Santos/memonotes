f0187b0638821dbba0c51cd7cf27bd904238d401
Joao-Vitor-Nicolau-dos-Santos <tinc3lle@gmail.com>
2024-10-25 11:05:14 -0300

    25/10/2024 - meio-dia

--- src/Paginas/Inicio/index.jsx
+++ src/Paginas/Inicio/index.jsx
@@ -1,5 +1,8 @@
 import Cabecalho from "../../Componentes/Cabecalho";
 import note from "../../assets/img/note.svg"
+import pencil from "../../assets/img/pencil.svg"
+import conection from "../../assets/img/conection.svg"
+import version from "../../assets/img/version.svg"
 import "./Inicio.css"
 import { NavLink } from "react-router-dom";
 
@@ -19,7 +22,16 @@ function Inicio() {
           <img src={note} alt="note" className="note_img" />
         </div>
         <div className="container">
-          
+          <div className="apresentacao">
+            <h1>Começe suas notas com a gente.</h1>
+            <h5>Transforme suas ideias em realidade!
+            Capture cada pensamento e faça anotações que impulsionam sua criatividade e organização.</h5>
+            <div className="img_apresentacao">
+              <img src={pencil} alt="note" className="note_img" />
+              <img src={conection} alt="note" className="note_img" />
+              <img src={pencil} alt="note" className="note_img" />
+            </div>
+          </div>
         </div>
       </>
     );
